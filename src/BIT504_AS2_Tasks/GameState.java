e541f9f0f1b1704f137b62122e5b60665f4c0b2b
jacqueline-coia <contactcoia@gmail.com>
2023-05-16 18:27:34 +1200

    Test Board commit to Experiment 1

--- src/BIT504_AS2_Tasks/GameState.java
+++ src/BIT504_AS2_Tasks/GameState.java
@@ -1,5 +1,11 @@
 package BIT504_AS2_Tasks;
 
 public enum GameState {
+	
+	CROSS_WON, // Indicates that Cross has won
+    NOUGHT_WON, // Indicates that Nought has won
+    DRAW, // Indicates that the game has ended in a draw
+    IN_PROGRESS, // Indicates that the game is still in progress
+    PLAYING // Indicates that the game is currently being played
 
 }
